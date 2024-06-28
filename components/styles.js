54434ea555a5de9b4386f465c011223bfdabcca8
achyu2003 <achyugeorge7744@gmail.com>
2024-06-29 00:17:21 +0530

    inital files

---
+++ components/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import {View} from 'react-native';
+
+export const colours = {
+    primary : "#ffffff",
+    secondary : "#E5E7EB",
+    tertiary : "#1F2937",
+    darklight : "#9CA3AF",
+    brand : "#6D28D9",
+    green : "#10B981",
+    red : "EF4444"
+}
+
+const {primary, secondary, tertiary, darklight, brand, green, red} = Colours;
+
+export const StyledContainer = styled.View`
+
+`
